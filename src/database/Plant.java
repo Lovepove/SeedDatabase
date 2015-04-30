cf3f9a7a16f10c765d5350eb5110fbf37f6e7683
Love Sjögren <lovepove@gmail.com>
2015-04-30 14:24:55 +0200

    first commit

---
+++ src/database/Plant.java
@@ -0,0 +1,86 @@
+package database;
+import java.sql.Time;
+import java.util.Date;
+
+
+public class Plant {
+
+	String name;
+	String purchasedAt;
+	Date purchaseDate;
+	Time sowTime;
+	Date sproutDate;
+	String location;
+	int grade;
+	String information;
+	String imagePath;
+
+	public Plant(String name) {
+		this.name = name;
+	}
+
+	public void setPurchasedAt(String purchasedAt) {
+		this.purchasedAt = purchasedAt;
+	}
+
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	public void setSowTime(Time sowTime) {
+		this.sowTime = sowTime;
+	}
+
+	public void setSproutDate(Date sproutDate) {
+		this.sproutDate = sproutDate;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	public void setInformation(String information) {
+		this.information = information;
+	}
+	
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	
+	public String getImagePath() {
+		return imagePath;
+	}
+	
+	public String getInformation() {
+		return information;
+	}
+	
+	public String getPurchasedAt() {
+		return purchasedAt;
+	}
+
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public Time getSowTime() {
+		return sowTime;
+	}
+
+	public Date getSproutDate() {
+		return sproutDate;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+}
