ee3c6afd54f4b67fddb723823a58ef59dd480063
Love Sjögren <lovepove@gmail.com>
2015-04-30 14:43:40 +0200

    first commit

--- src/GUI/plantView.java
+++ src/GUI/plantView.java
@@ -1,5 +1,14 @@
 package GUI;
 
-public class plantView {
+import java.util.Observable;
+import java.util.Observer;
+
+public class plantView implements Observer {
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		System.out.println("rawr! :3");
+		
+	}
 
 }
